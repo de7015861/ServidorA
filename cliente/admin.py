2918c9ec6e4dc06df5967e14c10005b63651d46d
Daniel <de7015861@gmail.com>
2018-06-03 21:00:41 -0500

    ServidorA V1.1

--- cliente/admin.py
+++ cliente/admin.py
@@ -8,8 +8,8 @@ class ClienteResource(resources.ModelResource):
 
     class Meta:
         model = Cliente
-        import_id_fields = ('isbn',)
-        fields = ('contacto', 'direccion_entrega', 'razon_social', 'rfc',
+        import_id_fields = ('id',)
+        fields = ('id', 'contacto', 'direccion_entrega', 'razon_social', 'rfc',
         'direccion', 'codigo_postal', 'ciudad', 'credito', 'correo', 'telefono')
 
 class ClienteAdmin(ImportExportModelAdmin):
